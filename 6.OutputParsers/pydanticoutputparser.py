7a178fba69d006d9685763d9bcc27c1473468df3
mihir <coolmihirbro@gmail.com>
2026-06-22 22:50:27 +0530

    add output parser implementations for JSON, Pydantic, and string formats

---
+++ 6.OutputParsers/pydanticoutputparser.py
@@ -0,0 +1,42 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+
+load_dotenv()
+
+# Define the model
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen3-Coder-30B-A3B-Instruct",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+class Person(BaseModel):
+
+    name: str = Field(description='Name of the person')
+    age: int = Field(gt=18, description='Age of the person')
+    city: str = Field(description='Name of the city the person belongs to')
+
+parser = PydanticOutputParser(pydantic_object=Person)
+
+
+template = PromptTemplate(
+    template='Generate the name, age and city of a fictional {place} person \n {format_instruction}',
+    input_variables=['place'],
+    partial_variables={'format_instruction':parser.get_format_instructions()}
+)
+
+asdf = template.invoke({'place':'japanese'})
+
+model_response = model.invoke(asdf)
+
+chain = template | model
+
+final_result = chain.invoke({'place':'sri lankan'})
+
+print(model_response.content)
+print(asdf)
+print(final_result.content)
